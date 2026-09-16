daa61b6df8ae6e49118533b2008b6998c5e189ae
KASURU RISHIKA <tranquilitypetrichor8021@gmail.com>
2026-09-16 23:42:21 +0530

    Time: 14 ms (92.75%) | Memory: 51.6 MB (19.27%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n=g.length;
+        int m=s.length;
+        int i=0,j=0,count=0;
+        while(i<n && j<m){
+            if(g[i]<=s[j]){
+                i++;
+                j++;
+                count++;
+            }else{
+                j++;
+            }
+        }
+        return count;
+        
+    }
+}
